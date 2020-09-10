cf36590106057a67f33bd65b24de2687e582961c
Dipjyoti Metia <dipjyotimetia@gmail.com>
2020-09-10 21:36:51 +1000

    added new integration tests

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.3.0
-	github.com/golang/mock v1.4.4
 	github.com/lib/pq v1.8.0
 	github.com/stretchr/testify v1.6.1
 	github.com/twinj/uuid v1.0.0
--- service/user-service_test.go
+++ service/user-service_test.go
@@ -1,20 +1,96 @@
 package service
 
 import (
+	"github.com/GoGinApi/v2/entity"
+	"github.com/GoGinApi/v2/mocks"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestService(t *testing.T)  {
-	//ctrl := gomock.NewController(t)
-	//
-	//defer ctrl.Finish()
-	//
-	//userResp1 := entity.User{
-	//	ID:       1,
-	//	Name:     "DemoName",
-	//	Location: "NewLocation",
-	//	Age:      20,
-	//}
-	//
-	//userResp := []entity.User{userResp1}
+func TestUserServices(t *testing.T) {
+	for _, c := range []TestCase{
+		{"Successfully create user", testCreateUser},
+		{"Successful reset password", testResetPassword},
+		{"Successful test login", testLogin},
+		{"Successful check user exist", testCheckUserExist},
+		{"Successful check and retrieve userid", testCheckAndRetrieveUserIDViaEmail},
+	} {
+		t.Run(c.name, c.test)
+	}
+}
+
+func testCreateUser(t *testing.T) {
+	mockRepo := new(mocks.MyMockedObject)
+
+	user := entity.Register{
+		Password: "Password1",
+		Email:    "test1@gmail.com",
+		Name:     "test1",
+	}
+
+	mockRepo.On("Create").Return(nil)
+
+	testService := NewUser(mockRepo)
+	res := testService.Create(user)
+	assert.Equal(t, res, nil)
+}
+
+func testResetPassword(t *testing.T) {
+	mockRepo := new(mocks.MyMockedObject)
+
+	reset := entity.ResetPassword{
+		Password: "Password1",
+	}
+
+	mockRepo.On("ResetPassword").Return(nil)
+
+	testService := NewUser(mockRepo)
+	res := testService.ResetPassword(reset)
+	assert.Equal(t, res, nil)
+}
+
+func testLogin(t *testing.T) {
+	mockRepo := new(mocks.MyMockedObject)
+
+	login := entity.Login{
+		Password: "Password1",
+		Email:    "test1@gmail.com",
+	}
+
+	mockRepo.On("Login").Return(nil)
+
+	testService := NewUser(mockRepo)
+	res := testService.Login("", "", "", "", "", login)
+	assert.Equal(t, res, nil)
+}
+
+func testCheckUserExist(t *testing.T) {
+	mockRepo := new(mocks.MyMockedObject)
+
+	user := entity.Register{
+		Password: "Password1",
+		Email:    "test1@gmail.com",
+		Name:     "test1",
+	}
+
+	mockRepo.On("CheckUserExist").Return(true)
+
+	testService := NewUser(mockRepo)
+	res := testService.CheckUserExist(user)
+	assert.Equal(t, res, true)
+}
+
+func testCheckAndRetrieveUserIDViaEmail(t *testing.T) {
+	mockRepo := new(mocks.MyMockedObject)
+
+	user := entity.CreateReset{
+		Email: "test1@gmail.com",
+	}
+
+	mockRepo.On("CheckAndRetrieveUserIDViaEmail").Return(1, true)
+
+	testService := NewUser(mockRepo)
+	res, res2 := testService.CheckAndRetrieveUserIDViaEmail(user)
+	assert.Equal(t, res, 1)
+	assert.Equal(t, res2, true)
 }
