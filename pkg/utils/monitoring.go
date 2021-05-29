7b5b87fb36734e0f176b60aaf82a0583a3707cd6
Dipjyoti Metia <dipjyotimetia@gmail.com>
2021-05-30 00:30:36 +1000

    adding newrelic

--- pkg/utils/monitoring.go
+++ pkg/utils/monitoring.go
@@ -2,14 +2,13 @@ package utils
 
 import (
 	"github.com/newrelic/go-agent/v3/newrelic"
-	"os"
 )
 
 // SetupNewRelic setup newrelic for apm
 func SetupNewRelic() (*newrelic.Application, error) {
 	app, err := newrelic.NewApplication(
 		newrelic.ConfigAppName("GoGinApi"),
-		newrelic.ConfigLicense(os.Getenv("NR_APIKEY")),
+		newrelic.ConfigLicense("eu01xxf5377b1860a6a52f52c9c20c86adadNRAL"),
 		newrelic.ConfigDistributedTracerEnabled(true),
 	)
 	if err != nil {
